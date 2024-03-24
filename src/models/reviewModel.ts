eb6a4c4d343ceddb2e551946ebd8a0d066c3398c
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-03-24 11:20:18 -0600

    Review model

---
+++ src/models/reviewModel.ts
@@ -0,0 +1,46 @@
+import {HydratedDocument, Schema, Types, model} from 'mongoose';
+import { IUser } from './userModel';
+
+
+export interface IReview
+{
+    review:string,
+    rating:number,
+    createdAt:Date,
+    tour: Types.ObjectId,
+    user: HydratedDocument<IUser>
+}
+
+
+const reviewSchema = new Schema<IReview>({
+    review: {
+        type:String,
+        required: [true, 'Reviews must have text']
+    },
+    rating: {
+        type:Number,
+        required:[true, 'Review must have a rating'],
+        min: [1, 'Rating must be at least 1'],
+        max: [5, 'Rating cannot be more than 5']
+    },
+    createdAt: {
+        type:Date,
+        default: Date.now()
+    },
+    tour:{
+        type: Schema.ObjectId,
+        ref:'Tour',
+        required:[true, 'Review must belong to a tour']
+    },
+    user:{
+        type: Schema.ObjectId,
+        ref:'User',
+        required: [true, 'Review must belong to a user']
+    }
+})
+
+
+const Review = model ('Review', reviewSchema);
+
+
+export default Review;
